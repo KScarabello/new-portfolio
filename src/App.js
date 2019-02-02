ef68c1141df72ce25e63df26b96a9511029e25af
KScarabello <kimberly.scarabello@gmail.com>
2019-02-01 20:33:32 -0700

    SUCCESS!!!
    Successfully setting image background depende

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import Header from './header/Header.js';
 import Body from './body/Body.js';
 import moment from 'moment';
 import succulents from './images/succulents.jpg';
+import tiles from './images/tiles.jpg';
+import pinkleaves from './images/pinkleaves.jpg';
+import coffee from './images/coffee.jpg';
+import ferns from './images/ferns.jpg';
+import hearts from './images/hearts.jpg';
+import graffiti from './images/graffiti.jpg';
 
 class App extends Component {
   constructor(props){
@@ -15,11 +21,13 @@ class App extends Component {
   componentDidMount = () => {
     this.setState({today: moment().format('dddd').toLowerCase()})
   }
+
+ 
   render() {
     return (
       <div>
         { this.state.today ? (
-          <AppContainer color="blue">
+          <AppContainer imagePath={days[this.state.today]}>
               <Header />
               <Body />
           </AppContainer>
@@ -34,13 +42,24 @@ class App extends Component {
 
 export default App;
 
+const days = {
+  monday: `${succulents}`,
+  tuesday: `${coffee}`,
+  wednesday: `${ferns}`,
+  thursday: `${hearts}`,
+  friday: `${graffiti}`,
+  saturday: `${tiles}`,
+  sunday: `${pinkleaves}`
+}
+
+
 
 //styled-component
 const AppContainer = styled('div')`
   width: 100vw;
   height: 100vh;
   padding-top: 72px;
-  background-image: linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ),url(${succulents});
+  background-image: linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ),url(${props => props.imagePath});
   background-repeat: round;
   display: flex;
   justify-content: center;
