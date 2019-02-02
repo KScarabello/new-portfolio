c791ff5979fd0c80e3d711edf3cade5b32f37306
KScarabello <kimberly.scarabello@gmail.com>
2019-02-01 22:57:15 -0700

    changed photos for backgrounds
    also, incorporated a photocredit component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Header from './header/Header.js';
 import Body from './body/Body.js';
 import moment from 'moment';
 import succulents from './images/succulents.jpg';
-import tiles from './images/tiles.jpg';
-import pinkleaves from './images/pinkleaves.jpg';
-import coffee from './images/coffee.jpg';
-import ferns from './images/ferns.jpg';
-import hearts from './images/hearts.jpg';
+import colorfulDesign from './images/colorfulDesign.jpg';
+import leaves from './images/leaves.jpg';
 import graffiti from './images/graffiti.jpg';
+import china from './images/china.jpg';
+import colorfulDesign2 from './images/colorfulDesign2.jpg';
+import mountainValley from './images/mountainValley.jpg';
 
 class App extends Component {
   constructor(props){
@@ -22,17 +22,27 @@ class App extends Component {
     this.setState({today: moment().format('dddd').toLowerCase()})
   }
 
- 
   render() {
+    const { today } = this.state;
     return (
       <div>
-        { this.state.today ? (
-          <AppContainer imagePath={days[this.state.today]}>
+        { today ? (
+          <AppContainer imagePath={days[today].path}>
               <Header />
-              <Body />
+              <PhotoCreditContainer>
+                    <p>Photograph by:  {'   '}
+                      <a 
+                        href={`https://unsplash.com/${days[today].photographer.unsplash}`}
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        {days[today].photographer.name}
+                        </a>
+                    </p>
+                </PhotoCreditContainer>
+              <Body photographer={days[today].photographer} />
           </AppContainer>
         ) : (
-          <h1>loading</h1>
+          <h1>hi there</h1>
         )
         }
       </div>
@@ -43,27 +53,76 @@ class App extends Component {
 export default App;
 
 const days = {
-  monday: `${succulents}`,
-  tuesday: `${coffee}`,
-  wednesday: `${ferns}`,
-  thursday: `${hearts}`,
-  friday: `${graffiti}`,
-  saturday: `${tiles}`,
-  sunday: `${pinkleaves}`
+  monday: {
+        path:`${succulents}`,
+        photographer:{
+          name: 'Yousef Espanioly',
+          unsplash: '@yespanioly'
+        }
+      },
+  tuesday: {
+        path: `${colorfulDesign}`,
+        photographer: {
+          name: 'Ashkan Forouzani',
+          unsplash: '@ashkfor121'
+        } 
+  },
+  wednesday: {
+        path: `${leaves}`,
+        photographer: {
+          name: 'Kyle Loftus',
+          unsplash: '@kalvisuals'
+        }
+  },
+  thursday: {
+        path: `${graffiti}`,
+        photographer: {
+          name: 'Renee Fisher',
+          unsplash: '@reneefisherandco'
+        }
+  },
+  friday: {
+        path: `${china}`,
+        photographer: {
+          name: 'Zhu Hongzhi',
+          unsplash: '@zhuzhutrain'
+        }
+  },
+  saturday: {
+        path: `${colorfulDesign2}`,
+        photographer:{
+          name: 'Prince Abid',
+          unsplash: '@princeabid708'
+        } 
+  },
+  sunday: {
+        path: `${mountainValley}`,
+        photographer: {
+          name: 'Elias Arnar',
+          unsplash: '@eliasarnars'
+        }
+  },
 }
-
-
 
 //styled-component
 const AppContainer = styled('div')`
   width: 100vw;
   height: 100vh;
-  padding-top: 72px;
   background-image: linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ),url(${props => props.imagePath});
-  background-repeat: round;
+  background-size: cover;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
 `
 
+const PhotoCreditContainer = styled('div')`
+    width:80vw;
+    height: 25px;
+    display: flex;
+    justify-content: flex-end;
+    color: whitesmoke;
+    a{
+      color: whitesmoke;
+    }
+`
--- src/body/Body.js
+++ src/body/Body.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import styled from '../../node_modules/@emotion/styled';
 
-const Body = () => (
+const Body = (photographer) => (
     <Container>
-        <Tabs />
+        <Tabs photogName={photographer.name} photogLink={photographer.unsplash} />
     </Container>
 );
 export default Body
-
 
 class Tabs extends React.Component{
     constructor(props){
@@ -17,14 +16,17 @@ class Tabs extends React.Component{
 
     tabSelect = (tabselected) => {this.setState({selectedTab: tabselected})};
     render(){
+        console.log('photographer', this.props.photogName)
         return(
-            <TabStyles>
-                <li onClick={() => this.tabSelect('about')}>About</li>
-                <li onClick={() => this.tabSelect('journal')}>What am I working on?</li>
-                <li onClick={() => this.tabSelect('lifeStory')}>My Life Story</li>                
-                <li onClick={() => this.tabSelect('myProjects')}>My Projects</li>
-                <li onClick={() => this.tabSelect('art')}>Art</li>
-            </TabStyles>
+            <div>
+                <TabStyles>
+                    <li onClick={() => this.tabSelect('about')}>About</li>
+                    <li onClick={() => this.tabSelect('journal')}>What am I working on?</li>
+                    <li onClick={() => this.tabSelect('lifeStory')}>My Life Story</li>                
+                    <li onClick={() => this.tabSelect('myProjects')}>My Projects</li>
+                    <li onClick={() => this.tabSelect('art')}>Art</li>
+                </TabStyles>
+            </div>
         )
     }
 }
@@ -33,14 +35,18 @@ class Tabs extends React.Component{
 const Container = styled('div')`
     width: 80vw;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    background-color: #2C232D;
+
 `
 const TabStyles = styled('ul')`
     display: flex;
     justify-content: space-between;
-    width: 80%;
+    align-items: center;
+    width: 80vw;
     list-style: none;
+    background-color: #2C232D;
     color: whitesmoke;
+    height: 72px;
 `
